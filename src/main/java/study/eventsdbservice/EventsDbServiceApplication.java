d2da31d212af9372660a2958d67943b73c800c70
almax07082005 <almaxim07082005@gmail.com>
2024-10-07 16:49:35 +0300

    project init

---
+++ src/main/java/study/eventsdbservice/EventsDbServiceApplication.java
@@ -0,0 +1,13 @@
+package study.eventsdbservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsDbServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventsDbServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/eventsdbservice/EventsDbServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package study.eventsdbservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsDbServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
